d208dc955f68adb935e3ed579656d4a5148790a5
kmario <mehamednews@gmail.com>
2023-10-23 09:22:36 +0100

    should fail

--- tests/inde.test.js
+++ tests/inde.test.js
@@ -5,11 +5,11 @@ const supertest = require('supertest');
 const request = supertest(app);
 
 describe('API Endpoint Testing', () => {
-  it('should respond with a greeting message', async () => {
-    // Make a GET request to the /api/greet endpoint
-    const response = await request.get('/');
+	it('should respond with a greeting message', async () => {
+		// Make a GET request to the /api/greet endpoint
+		const response = await request.get('/');
 
-    // Assert the response status code is 200 (OK)
-    expect(response.status).toBe(200);
-  });
+		// Assert the response status code is 200 (OK)
+		expect(response.status).toBe(201);
+	});
 });
